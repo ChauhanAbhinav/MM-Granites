5942a022edbc46da8d20e8b91781113965e52d00
Mannu Kumar <mannukumar7891@gmail.com>
2018-09-17 00:17:17 +0530

    index file added

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Marble</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="css/header-footer.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
 
@@ -15,5 +15,93 @@ include 'header.php';
 ?>
 
 </div>
+<section class="main_banner" style="background-image:url('assets/img/home.jpg');">
+<div class="container" id="banner">
+<div class="row">
+<div class="col-sm-12 col-md-12">
+<center>
+	<p id="banner_text">Welcome to the company
+		<br>
+	
+</p>
+<h3 style="color:white">
+		MM Granite.We are the leader dealer in all kinds of marbel product designs across Rajasthan..
+	</h3>	
+</center>
+<br>
+<br>
+<center>
+	<a href="#"><button class="btn btn-info" style="zoom:150%;opacity:1">Explore More</button></a>
+</center>
+</div>
+</div>
+ </section>
+ <section class="why_us">
+<div class="container">
+<div class="row">
+<div class="col-sm-12 col-md-12 col-xs-12">
+  <center>
+  	<h1 style="font-weight:900">Why Us</h1>
+
+  </center>
+  <p class="why_us_text">
+    addfgdfdvfdgr; dfvb f'l,'d fg xfbv fdlbv dfg  fdgvbd dlfbdfgbd fdgvs dkjm;
+  </p>
+</div>
+
+
+</div>
+
+ </section>
+ <section style="background-image:url('assets/img/work.jpg');">
+ 	<div class="container" >
+<div class="col-md-4 col-sm-12">
+<center>
+	<img src="assets/img/quality2.png" class="img-responsive img-circle" style="zoom:50%;margin-top:20px" >
+</center>
+<center style="color: white;font-weight: 700;font-size: 2.5em">High Quality</center>
+<p style="color: white">
+	
+
+	License: Distributed under the Lesser General Public License (LGPL)     
+           http://www.gnu.org/copyleft/lesser.html                        
+  This program is distributed in the hope that it will be useful - WITHOUT  
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or     
+  FITNESS FOR A PARTICULAR PURPOSE
+</p>
+</div>
+<div class="col-md-4 col-sm-12">
+<center>
+	<img src="assets/img/quality2.png" class="img-responsive img-circle" style="zoom:50%;margin-top:20px" >
+</center>
+<center style="color: white;font-weight: 700;font-size: 2.5em">High Quality</center>
+<p style="color: white">
+	
+
+	License: Distributed under the Lesser General Public License (LGPL)     
+           http://www.gnu.org/copyleft/lesser.html                        
+ This program is distributed in the hope that it will be useful - WITHOUT  
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or     
+ FITNESS FOR A PARTICULAR 
+</p>
+</div>
+<div class="col-md-4 col-sm-12">
+<center>
+	<img src="assets/img/quality2.png" class="img-responsive img-circle" style="zoom:50%;margin-top:20px" >
+</center>
+<center style="color: white;font-weight: 700;font-size: 2.5em">High Quality</center>
+<p style="color: white">
+	
+
+	License: Distributed under the Lesser General Public License (LGPL)     
+           http://www.gnu.org/copyleft/lesser.html                        
+ This program is distributed in the hope that it will be useful - WITHOUT  
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or     
+ FITNESS FOR A PARTICULAR 
+</p>
+</div>
+
+
+ </section>
 </body>
 </html>
