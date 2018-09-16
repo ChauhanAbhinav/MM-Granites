1ecb0a4a391ac2bc20775265296fb32808912d3b
Abhinav Chauhan <abhinavchauhan25493@gmail.com>
2018-09-17 00:22:09 +0530

    header-footer css added

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <title>Marble</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/header-footer.css">
 </head>
 <body>
 
