405ac4eb394e3d9b65f9e7bdef34174d4d6f1307
Abhinav Chauhan <abhinavchauhan25493@gmail.com>
2018-09-16 23:47:48 +0530

    Initial commit

---
+++ header.php
@@ -0,0 +1,15 @@
+<div class="header">
+	<div class="header-inside">
+		<div class="logo-wrapper">
+			<img class="logo" src="assets/img/logo-32.png">
+			<span class="mmGranites">MM Granites</span>
+		</div>
+		<div class="nav-bar">
+			<a>Home</a>
+			<a>Products</a>
+			<a>Projects</a>
+			<a>About Us</a>
+			<a>Contact Us</a>
+		</div>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Marble</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/header-footer.css">
+</head>
+<body>
+
+<div class="container-fluid">
+<?php 
+include 'header.php';
+?>
+
+</div>
+</body>
+</html>
