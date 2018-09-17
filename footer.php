4f50344c2fb069c358b87a5eb3cd1d32830a0738
Abhinav Chauhan <abhinavchauhan25493@gmail.com>
2018-09-18 00:20:04 +0530

    updated index and footer

---
+++ footer.php
@@ -0,0 +1,41 @@
+<div class="footer">
+	<div class="row inside-footer">
+		<div class="col-sm-4 address-block">
+			<div class="logo-wrapper-footer">
+				<img class="logo" src="assets/img/logo-32.png">
+				<span class="mmGranites mmGranites-footer">MM Granites</span>
+			</div>
+			<div class="address">
+				Nullam ac tortor ut massa fringilla semper non sit amet purus. Donec non nisl non dolor ultrices aliquam. Phasellus elementum sapien sed blandit finibus.  
+			</div>	
+		</div>
+		<div class="col-sm-4 query-block">
+			<span class="footer-heading">Enquery</span>
+			<form action="#" class="query-form">
+				<div class="form-group">
+            <input type="number" name="number" class="form-control" placeholder="Enter Mobile No.">
+ 
+			</div>
+			<div class="form-group">
+            <input type="email" name="email" class="form-control" placeholder="Enter Email">
+ 
+			</div>
+			<div class="form-group">
+            <textarea rows="4" cols="20" placeholder="Your Query" name="query" class="form-control">  </textarea>
+  			</div>
+			<div class="form-group">
+				<input type="submit" name="query-submit" class="btn btn-default query-btn">
+			</div>
+			</form>
+		</div>
+		<div class="col-sm-4 social-block">
+			<span class="footer-heading">Social Link</span>
+			<div class="social-icon-block">
+				<img src="assets/img/fb.png" class="social-icon">
+				<img src="assets/img/twitter.png" class="social-icon">
+				<!-- <img src="assets/img/fb.png" class="social-icon"> -->
+			</div>
+		</div>
+	</div>
+	<span class="copyright">&copy;Copy right 2018. All right reserved.</span>
+</div>
