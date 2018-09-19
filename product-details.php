51cd876a4beafbd9b773a361cae260b686bc975f
Mannu Kumar <mannukumar7891@gmail.com>
2018-09-19 15:33:52 +0530

    5 changes done

---
+++ product-details.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>MM Granites</title>
+<link rel="apple-touch-icon" sizes="180x180" href="assets/img/logo-32.png">
+    <link rel="icon" type="image/png" href="assets/img/logo-32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="assets/img/logo-32.png" sizes="16x16">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/css_product.css">
+<link rel="stylesheet" type="text/css" href="css/header-footer.css">
+</head>
+<body>
+
+<?php 
+include 'header.php';
+?>
+
+<section style="background-image: url('assets/img/products.jpg');height:150px ">
+<div class="container-fluid" id="product_banner"> 
+	<div class="row">
+		<div class="col-sm-3">
+			<h1 style="color: white;margin-top:50px">Products</h1>
+			<h5 ><a style="color: white;text-decoration: none" href="index.php">Home /</a><a style="color: white;text-decoration: none" href="#">Products</a>
+			</h5>
+		</div>
+
+	</div>
+</div>
+</section>
+
+<section>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="jumbo">
+					<center><h3>Our Products are Our Main Strength</h3></center>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section style="margin-top: 20px;">	
+<div class="container-fluid">
+<div class="row">
+<div class="col-sm-4">
+
+
+
+</div>
+<div class="col-sm-8">
+
+</div>
+<div class="container-fluid">
+	<div class="jumbo">
+	<center>Space for Pagination</center>
+	</div>
+</div>
+
+</section>
+ <?php
+include 'footer.php';
+ ?>
+</body>
+</html>
